65f71fd4b3a635a8902aa22addf817fe118b43fd
quyixiao <2621048238@qq.com>
2020-02-17 16:09:49 +0800

    提交修改

--- post/models.py
+++ post/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+from user.models import User
+
+
+class Post(models.Model):
+    class Meta:
+        db_table = 'bl_post'
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, null=False)
+    pubdate = models.DateTimeField(null=False)
+    # 作者
+    # author_id = models.IntegerField(null=False)
+    author = models.ForeignKey(User)
+
+    # 内容
+
+    def __repr__(self):
+        return "<Post {} {} >".format(self.id, self.title)
+
+    __str__ = __repr__
+
+
+class Content(models.Model):
+    class Meta:
+        db_table = 'bl_content'
+
+    # id 可以不写，主键django帮你创建一个pk
+    post = models.OneToOneField(Post, to_field='id')  # post_id
+    content = models.TextField(null=False)
+
+    def __repr__(self):
+        return "<Content {} {} >".format(self.id, self.content[:40])
+
+    __str__ = __repr__
--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse, Http404
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def pub(request: HttpRequest):
+    return HttpResponse(b'pub')
+
+
+def get(request: HttpRequest, id):
+    print(id)
+    return HttpResponse(b'get')
+
+
+def getall(request: HttpRequest):
+    return HttpResponse(b'getall')
